ee57da1d4cdf7778f4a7e0acaff16530207666f4
4283246 <102957536+4283246@users.noreply.github.com>
2022-08-09 14:45:25 +0200

    Add project files.

---
+++ GoogleMapsApp/GoogleMapsApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoogleMapsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DisplayLocation();
+        }
+        public async void DisplayLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+                if (location != null)
+                {
+                    Position p = new Position(location.Latitude, location.Longitude);
+                    MapSpan mapSpan = MapSpan.FromCenterAndRadius(p, Distance.FromKilometers(.444));
+                    map.MoveToRegion(mapSpan);
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    //map.Pins.Add(pin);
+                }
+            }
+            catch { }
+        }
+    }
+}
